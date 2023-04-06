1756a749717c73292c8fde45cc622beae33e1143
YogeshGurjar011 <113578887+YogeshGurjar011@users.noreply.github.com>
2023-04-06 11:40:52 +0530

    change get all function by yogesh

--- controller/admin/adminScreenController.js
+++ controller/admin/adminScreenController.js
@@ -1002,11 +1002,11 @@ const adminGetAllRides = async (req, res) => {
           ride_id: ride._id,
           rider_name: riderName,
           driver_name: driverName,
-          pickup_address: `<span style="height: 20px; width: 20px; background-color: green; border-radius: 50%; display: inline-block;"></span> ${ride.pickupLocation}`,
-          dropoff_address: `<span style="height: 20px; width: 20px; background-color: red; border-radius: 50%; display: inline-block;"></span> ${ride.destinationLocation}`,
+          pickup_address: `<span style="height: 10px; width: 10px; background-color: green; border-radius: 50%; display: inline-block;"></span> ${ride.pickupLocation}`,
+          dropoff_address: `<span style="height: 10px; width: 10px; background-color: red; border-radius: 50%; display: inline-block;"></span> ${ride.destinationLocation}`,
           ride_fare: ride.fare,
           date: ride.scheduledDate,
-          status: `<button style="background-color: ${statusColor};  border-radius:4px ; color:#FFFFFF; fontFamily: Roboto; fontStyle: normal; fontWeight: 500; fontSize: 12px; lineHeight: 14px; width: 100px; height: 30px;">${ride.status}</button>`
+          status: `<button style="background-color: ${statusColor};  border-radius:6px ; color:#FFFFFF; fontFamily: Roboto; fontStyle: normal; fontWeight: 500; fontSize: 12px; lineHeight: 14px; width: 100px; height: 30px;">${ride.status}</button>`
         };
       });
   
