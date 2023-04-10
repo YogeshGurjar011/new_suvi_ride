fd98d09f105756755a14e09c3ab49602b9f6b453
YogeshGurjar011 <113578887+YogeshGurjar011@users.noreply.github.com>
2023-04-10 10:28:39 +0530

    change driver update current location routes

--- routes/driverRoutes/driverRoutes.js
+++ routes/driverRoutes/driverRoutes.js
@@ -44,7 +44,7 @@ router.put('/driver/updateDriverStatus/:_id',driverController.updateDriverStatus
 
 router.get('/driver/totalDrivers',driverController.totalDrivers);
 
-router.put('/driver/updateCurrentLocation/:_id',driverValidation,driverController.updateDriverCurrentLocation);
+router.put('/driver/updateCurrentLocation',driverValidation,driverController.updateDriverCurrentLocation);
 
 router.delete('/driver/deleteDriver/:_id',driverController.deleteDriver);
 
