44a47fbc583dd002b2ff9a2c99e4ed3f45d00069
preetiyadav30 <py132430@gmail.com>
2023-04-18 15:33:49 +0530

    modify driverRouter and driverController

--- controller/driver/driverController.js
+++ controller/driver/driverController.js
@@ -2729,7 +2729,7 @@ const acceptRideRequest = async (req, res) => {
                         pickupLocation: findRideStatus.pickupLocation,
                         destinationLocation: findRideStatus.destinationLocation,
                         paymentMethod: findRideStatus.paymentMethod,
-                         confirmOtp: rideAccepted.confirmOtp,
+                        confirmOtp: rideAccepted.confirmOtp,
                         message: "Ride accepted successfully",
                         nextScreen: "Navigate to pickup point Screen",
                     });
