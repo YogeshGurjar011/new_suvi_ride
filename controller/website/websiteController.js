d341baf16cd042a538ffbda4129f27bc8851abba
preetiyadav30 <py132430@gmail.com>
2023-04-19 12:56:45 +0530

    modify websiteController

--- controller/website/websiteController.js
+++ controller/website/websiteController.js
@@ -129,75 +129,69 @@ function deg2rad(deg) {
 // contact box website
 const contactUs = async (req, res) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
       port: 587,
       requireTLS: true,
       secure: false,
       auth: {
-        user: "preeti@rslsofttech.com",
-        pass: "Indore@1234",
+        user: 'hr@rslsofttech.com',
+        pass: 'upennynhqtgpdybp'
       },
       tls: {
-        rejectUnauthorized: false,
-      },
-    });
-    const mailOptions = {
-      from: "preeti@rslsofttech.com",
-      to: "preeti@rslsofttech.com",
-      subject: "Contact Us",
-      html: `
-        <p>You have received a new email from suvi website.</p>
-        <table style="width:50%">
-          <tr>
-            <td><b>Name</b></td>
-            <td>${req.body.name}</td>
-          </tr>
-          <tr>
-            <td><b>Email</b></td>
-            <td>${req.body.email}</td>
-          </tr>
-          <tr>
-            <td><b>Subject</b></td>
-            <td>${req.body.subject}</td>
-          </tr>
-          <tr>
-            <td><b>Message</b></td>
-            <td>${req.body.message}</td>
-          </tr>
-        </table>
-      `,
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        res.status(400).send({
-          success: false,
-          message: "Mail not sent",
-        });
-      } else {
-        const customerMail = new contactUsModel({
-          Name: req.body.name,
-          Email: req.body.email,
-          Subject: req.body.subject,
-          Message: req.body.message,
-        });
-        customerMail.save((err, finalResult) => {
-          if (err) {
-            res.status(500).send({
-              success: false,
-              message: "Error in saving the contact message",
-              error: err.message,
-            });
-          } else {
-            res.status(200).send({
-              success: true,
-              message: "Mail sent successfully",
-              newUser: finalResult,
-            });
-          }
-        });
+        rejectUnauthorized: false
       }
     });
+    var mailOptions = {
+      from: 'hr@rslsofttech.com',
+      to: 'hr@rslsofttech.com',
+      subject: `Contact Us`,
+      html: `
+      <p>You received new eamil from rsl website.</p>
+      <table style="width:50%">
+      <tr>
+        <td><b>Name</b></td>
+        <td>${request.body.name}</td>
+      
+      </tr>
+      <tr>
+      <td><b>Email</b></td>
+      <td>${request.body.email}</td>
+    
+    </tr>
+    <tr>
+    <td><b>Subject</b></td>
+    <td>${request.body.subject}</td>
+  </tr>
+  <tr>
+  <tr>
+  <td><b>Message</b></td>
+  <td>${request.body.message}</td>
+  
+  </tr>
+    </table>`
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+     
+      const user = new User({
+        name: request.body.name,
+        email:request.body.email,
+        phone:request.body.phone,
+        message:request.body.message,
+        candidates_resume:request.file.filename
+      })
+
+      user.save()
+      .then(result => {
+        response.status(200).send({success:true, msg: "successfully send mail",new_user:result})
+      }).catch(e => {
+        response.status(400).send({success:false,error:e});
+      })
+    }
+  });
   } catch (error) {
     res.status(500).send({
       success: false,
@@ -206,6 +200,99 @@ const contactUs = async (req, res) => {
     });
   }
 };
+
+
+// import User from "../models/userModel.js";
+// import nodemailer from 'nodemailer';
+// import { Result } from "express-validator";
+
+// export async function insert(request, response) {
+ 
+//   var transporter = nodemailer.createTransport({
+//     host: 'smtp.gmail.com',
+//     port: 587,
+//     requireTLS: true,
+//     secure: false,
+//     auth: {
+//       user: 'hr@rslsofttech.com',
+//       pass: 'upennynhqtgpdybp'
+//     },
+//     tls: {
+//       rejectUnauthorized: false
+//     }
+//   });
+
+//   var mailOptions = {
+//     from: 'hr@rslsofttech.com',
+//     to: 'hr@rslsofttech.com',
+//     subject: `Contact Us`,
+//     html: `
+//     <p>You received new eamil from rsl website.</p>
+//     <table style="width:50%">
+//     <tr>
+//       <td><b>Name</b></td>
+//       <td>${request.body.name}</td>
+    
+//     </tr>
+//     <tr>
+//     <td><b>Email</b></td>
+//     <td>${request.body.email}</td>
+  
+//   </tr>
+//   <tr>
+//   <td><b>Phone</b></td>
+//   <td>${request.body.phone}</td>
+// </tr>
+// <tr>
+// <td><b>Candidates_resume</b></td>
+// <p><a href="http://localhost:6700/candidates_resume/${request.file.filename}">link for ${request.body.name}'s resume</a></p>
+// </tr>
+// <tr>
+// <td><b>Message</b></td>
+// <td>${request.body.message}</td>
+
+// </tr>
+//   </table>`
+//   };
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+     
+//       const user = new User({
+//         name: request.body.name,
+//         email:request.body.email,
+//         phone:request.body.phone,
+//         message:request.body.message,
+//         candidates_resume:request.file.filename
+//       })
+
+//       user.save()
+//       .then(result => {
+//         response.status(200).send({success:true, msg: "successfully send mail", _data: `http:localhost:4000/candidates_resume/${request.file.filename}` ,
+//       new_user:result})
+//       }).catch(e => {
+//         response.status(400).send({success:false,error:e});
+//       })
+//     }
+//   });
+
+// }
+// export async function fetchAll(request, response) {
+//   try {
+//     const data = await User.find()
+//     response.json(data);
+//   } catch (error) {
+//     response.json({ message: 'Something went wrong' });
+//   }
+// }
+
+
+
+
+
+
+
 
 module.exports = {
   getFare,showFareToUsers,contactUs
